981fe94b104a86c632b51d4ac68c9fdc3a765e45
JSlander <jhovanslander@hotmail.com>
2023-01-16 18:50:22 -0600

    debug commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,31 @@
-$(document).ready(function() {
-  $(".saveBtn").on("click", function () { // actually works now
+var todayDate = moment().format("dddd, MMM D YYYY");
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+  // actually works now
+  $(".saveBtn").on("click", function () {
     var eventText = $(this).siblings(".description").val();
     var date = $(this).parent().attr("id");
     localStorage.setItem(date, eventText);
   });
+  function saveDate() {
+    var newDate = dayjs().hour(12);
+    $(".time-block").each(function () {
+      var currentTime = parseInt($(newDate).attr("id").split("hour")[1]);
+      if (currentTime < todayDate) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (currentTime === todayDate) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
+  }
+  saveDate();
 });
-
-// does not work
-/* if (document.readyState == 'complete') {
-    saveDate();
-} else {
-    document.onreadystatechange = function () {
-        if (document.readyState === "complete") {
-            saveDate();
-        }
-    }
-} */
-$("#currentDay").html(todayDate);
