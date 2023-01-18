1d358dedcb217aa146eb4314b67fe768144b0486
JSlander <jhovanslander@hotmail.com>
2023-01-17 21:42:00 -0600

    cleaned up js with for loop, updated readme.

--- Assets/script.js
+++ Assets/script.js
@@ -2,16 +2,22 @@
 var todayDate = dayjs().format("dddd, MMM D YYYY");
 $("#currentDay").html(todayDate);
 
+// storing data to local storage with button click
 $(document).ready(function () {
+  // function only runs when page is ready
   $(".saveBtn").on("click", function () {
     var eventText = $(this).siblings(".description").val();
-    var date = $(this).parent().attr("id");
-    localStorage.setItem(date, eventText);
+    var timeSlot = $(this).parent().attr("id");
+    localStorage.setItem(timeSlot, eventText);
   });
+  // making current hour visible in page
   function saveDate() {
     var hour = dayjs().hour();
+    // get real time
     $(".time-block").each(function () {
-      var sectionHour = ($(this).attr("id").split("hour")[1]);
+      var sectionHour = ($(this).attr("id").split("-")[1]);
+      // compare real time to hour id of each container,
+      // then change class of container to reflect "past" "present" or "future"
       if (sectionHour < hour) {
         $(this).removeClass("present");
         $(this).addClass("past");
@@ -21,30 +27,10 @@ $(document).ready(function () {
       }
     });
   }
-  $("#1 .description").val(localStorage.getItem("hour1"));
-  $("#2 .description").val(localStorage.getItem("hour2"));
-  $("#3 .description").val(localStorage.getItem("hour3"));
-  $("#4 .description").val(localStorage.getItem("hour4"));
-  $("#5 .description").val(localStorage.getItem("hour5"));
-  $("#6 .description").val(localStorage.getItem("hour6"));
-  $("#7 .description").val(localStorage.getItem("hour7"));
-  $("#8 .description").val(localStorage.getItem("hour8"));
-  $("#9 .description").val(localStorage.getItem("hour9"));
-  $("#10 .description").val(localStorage.getItem("hour10"));
-  $("#11 .description").val(localStorage.getItem("hour11"));
-  $("#12 .description").val(localStorage.getItem("hour12"));
-  $("#13 .description").val(localStorage.getItem("hour13"));
-  $("#14 .description").val(localStorage.getItem("hour14"));
-  $("#15 .description").val(localStorage.getItem("hour15"));
-  $("#16 .description").val(localStorage.getItem("hour16"));
-  $("#17 .description").val(localStorage.getItem("hour17"));
-  $("#18 .description").val(localStorage.getItem("hour18"));
-  $("#19 .description").val(localStorage.getItem("hour19"));
-  $("#20 .description").val(localStorage.getItem("hour20"));
-  $("#21 .description").val(localStorage.getItem("hour21"));
-  $("#22 .description").val(localStorage.getItem("hour22"));
-  $("#23 .description").val(localStorage.getItem("hour23"));
-  $("#24 .description").val(localStorage.getItem("hour24"));
-
+  // read local storage and apply stored text to textboxes for each element
+  for (let index = 1; index < 25; index++) {
+    $(`#hour-${index} .description`).val(localStorage.getItem(`hour-${index}`))
+  }
+  
   saveDate();
 });
